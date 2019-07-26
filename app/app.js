c8b9eb04fd853f1b62642f89bfe44c818b4dacc8
thiagogalvao <tsoares@Aubay.localdomain>
2019-07-26 13:19:56 +0100

    Add comment

--- app/app.js
+++ app/app.js
@@ -2,9 +2,9 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
  
+// Include here the require and use of your API Module.
 var testeApi = require('./api/v1/test');
-
 app.use('/test', testeApi);
-app.listen(port);
 
+app.listen(port);
 console.log('RESTful API server started on: ' + port);
